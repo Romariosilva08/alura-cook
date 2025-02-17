04c34243f5010b71f03c1d87d942a5c5d97bf6a8
Romário <Romrio@DESKTOP-T6S5O68>
2025-02-17 20:44:21 -0300

    Finalizando projeto alura cook em svelte

---
+++ src/lib/components/Rodape.svelte
@@ -0,0 +1,20 @@
+<footer>
+    <img src="/icones/registered.png" alt="Ícone marca registrada" class="registered"/>
+</footer>
+
+<style>
+    footer {
+        background: url("imagens/footer.png");
+        height: 100px;
+        font-weight: 700;
+        color: var(--branco);
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .registered {
+        margin-right: 6px;
+    }
+</style>
